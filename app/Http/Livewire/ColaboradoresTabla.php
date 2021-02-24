e8997b7674935fcc72855638ecfae32019086c9a
Alexxlml <inkisidor_117@hotmail.com>
2021-02-24 17:30:26 -0600

    Modificaciones a la tabla
    - Se agregaron el campo de búsqueda así como el select para seleccionar la paginación manualmente dentro del componente ColaboradoresTabla.
    
    - Se modificó la vista del componente ColaboradoresTabla añadiendo un footer con la paginación así como un campo de búsqueda y un select para seleccionar la páginación manualmente

--- app/Http/Livewire/ColaboradoresTabla.php
+++ app/Http/Livewire/ColaboradoresTabla.php
@@ -2,14 +2,37 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\User;
+use App\Models\Colaborador;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ColaboradoresTabla extends Component
 {
+    use WithPagination;
+    public $search, $perPage='5';
+
     public function render()
     {
-        $users = User::latest('id')->get();
-        return view('livewire.colaboradores-tabla', compact('users'));
+        if ($this->search == 'activo') {
+            return view('livewire.colaboradores-tabla', [
+                'colaboradores' => Colaborador::where('no_colaborador', 'LIKE', "%{$this->search}%")
+                    ->orWhere('nombre', 'LIKE', "%{$this->search}%")
+                    ->orWhere('estado_colaborador', 1)
+                    ->paginate($this->perPage)
+            ]);
+        } else if ($this->search == 'inactivo') {
+            return view('livewire.colaboradores-tabla', [
+                'colaboradores' => Colaborador::where('no_colaborador', 'LIKE', "%{$this->search}%")
+                    ->orWhere('nombre', 'LIKE', "%{$this->search}%")
+                    ->orWhere('estado_colaborador', 0)
+                    ->paginate($this->perPage)
+            ]);
+        }
+
+        return view('livewire.colaboradores-tabla', [
+            'colaboradores' => Colaborador::where('no_colaborador', 'LIKE', "%{$this->search}%")
+                ->orWhere('nombre', 'LIKE', "%{$this->search}%")
+                ->paginate($this->perPage)
+        ]);
     }
 }
--- resources/views/livewire/colaboradores-tabla.blade.php
+++ resources/views/livewire/colaboradores-tabla.blade.php
@@ -5,49 +5,77 @@
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead class="bg-gray-50">
                         <tr>
-                            <th scope="col"
-                                class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-                                Nombre
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-                                No. Colaborador </th>
-                            <th scope="col"
-                                class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-                                Estado
-                            </th>
-                            <th scope="col" class="relative px-6 py-3 ">
-                                <span class="sr-only">Edit</span>
-                            </th> 
+                            <div class="grid grid-cols-4">
+                                <div class=" col-span-1 flex px-2 py-2 bg-white border-t border-gray-200 sm:px-3">
+                                    <select wire:model='perPage'
+                                        class=" border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mr-4">
+                                        <option value="5">5 por página</option>
+                                        <option value="10">10 por página</option>
+                                        <option value="25">25 por página</option>
+                                        <option value="50">50 por página</option>
+                                        <option value="100">100 por página</option>
+                                    </select>
+                                </div>
+                                <div class=" col-span-3 flex px-2 py-2 bg-white border-t border-gray-200 sm:px-3">
+                                    <input wire:model="search" type="text" placeholder="Buscar"
+                                        class="w-full col-span-3 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                </div>
+                            </div>
                         </tr>
+                        @if ($colaboradores->count())
+                            <tr>
+                                <th scope="col"
+                                    class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                    Nombre
+                                </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                    No. Colaborador </th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
+                                    Estado
+                                </th>
+                                <th scope="col" class="relative px-6 py-3 ">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200">
-                        @foreach ($users as $user)
-                            <tr>
+                        @foreach ($colaboradores as $colaborador)
+                            <tr class="h-30">
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <div class="flex items-center">
-                                        <div class="flex-shrink-0 w-10 h-10">
-                                            <img class="w-10 h-10 rounded-full"
-                                                src="{{ $user->profile_photo_url }}"
-                                                alt="{{ $user->name }}">
+                                        <div class="flex-shrink-0 w-25 h-25">
+                                            <img class="w-10 rounded shadow h-35"
+                                                src="{{ asset('storage') . '/' . $colaborador->foto }}"
+                                                alt="{{ $colaborador->nombre }}">
                                         </div>
                                         <div class="ml-4">
                                             <div class="text-sm font-medium text-gray-900">
-                                                {{ $user->name }}
+                                                {{ $colaborador->nombre }} {{ $colaborador->ap_paterno }}
+                                                {{ $colaborador->ap_materno }}
                                             </div>
                                         </div>
                                     </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
                                     <div class="text-sm text-gray-900">
-                                        {{ $user->email }}
+                                        {{ $colaborador->no_colaborador }}
                                     </div>
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
-                                    <span
-                                        class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
-                                        Activo
-                                    </span>
+                                    @if ($colaborador->estado_colaborador == 1)
+                                        <span
+                                            class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
+                                            Activo
+                                        </span>
+                                    @else
+                                        <span
+                                            class="inline-flex px-2 text-xs font-semibold leading-5 text-red-800 bg-red-100 rounded-full">
+                                            Inactivo
+                                        </span>
+                                    @endif
+
                                 </td>
                                 <td class="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
                                     <a href="#"
@@ -62,6 +90,15 @@
                         <!-- More items... -->
                     </tbody>
                 </table>
+                <div class="px-4 py-3 bg-white border-t border-gray-200 sm:px-6">
+                    {{ $colaboradores->links() }}
+                </div>
+            @else
+                <div class="px-4 py-3 bg-white border-t border-gray-200 sm:px-6">
+                    <h6 class="text-center text-gray-500">No se encontró a ningún campo que coincida con:
+                        "{{ $search }}"</h6>
+                </div>
+                @endif
             </div>
         </div>
     </div>
