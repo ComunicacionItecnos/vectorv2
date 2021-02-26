5e57b22cb4ab4f2df8b16c37f8570948f8df1a4f
Alexxlml <inkisidor_117@hotmail.com>
2021-02-26 10:39:24 -0600

    Cambio de Logo
    Se reemplazó el logo de Vector por el de Factor Toolkit
    Se ajustó la medida del logo

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-12 mx-auto w-25">
+    <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-25 mx-auto w-25">
 </a>
