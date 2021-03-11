db5b552ff40cf3049aae1fc9372a36e7ff19dc3a
Alexxlml <inkisidor_117@hotmail.com>
2021-03-11 15:51:14 -0600

    Cambio en tabla colaborador
    Se disminuyó el padding en x a 2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
     </x-slot>
 
     <div class="py-6">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-4">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-2">
             <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
                 <livewire:colaboradores-tabla></livewire:colaboradores-tabla>
             </div>
