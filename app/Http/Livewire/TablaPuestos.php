d68dec3a0ed6a9a45ccd7a041a0df8012c0a3aca
Alexxlml <inkisidor_117@hotmail.com>
2021-03-24 16:34:31 -0600

    Creación componente TablaPuestos
    - Se creó el componente TablaPuestos
    - Se creó la vista tablaPuestos

---
+++ app/Http/Livewire/TablaPuestos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TablaPuestos extends Component
+{
+    public function render()
+    {
+        return view('livewire.tabla-puestos');
+    }
+}
