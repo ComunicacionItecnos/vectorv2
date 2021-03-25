1e0dc5c665f4fe24e004d79c9986197d21259847
Alexxlml <inkisidor_117@hotmail.com>
2021-03-25 13:13:25 -0600

    Responsive en Control Center

--- resources/views/livewire/control-center.blade.php
+++ resources/views/livewire/control-center.blade.php
@@ -18,64 +18,64 @@
                     <tbody class="bg-white divide-y divide-gray-200">
                         @if(auth()->user()->role_id == 1 | auth()->user()->role_id == 3)
                         <tr>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="px-6 py-4 whitespace-nowrap sm:text-base md:sm:text-base md:text-sm text-gray-500">
                                 Área
                             </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="sm:px-2 md:px-6 py-4 whitespace-nowrap sm:text-base md:text-sm text-gray-500">
                                 <a href="{{route('tabla-area')}}"
-                                class="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800"
+                                class="inline-flex items-center h-8 px-4 m-2 sm:text-base md:text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800"
                                 >Mostrar</a>
                             </td>
                         </tr>
                         @endif
                         @if(auth()->user()->role_id == 1 | auth()->user()->role_id == 6)
                         <tr>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="px-6 py-4 whitespace-nowrap sm:text-base md:text-sm text-gray-500">
                                 Claves de radio
                             </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="sm:px-2 md:px-6 py-4 whitespace-nowrap sm:text-base md:text-sm text-gray-500">
                                 <a href="{{route('tabla-claves-radio')}}"
-                                    class="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800">Mostrar</a>
+                                    class="inline-flex items-center h-8 px-4 m-2 sm:text-base md:text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800">Mostrar</a>
                             </td>
                         </tr>
                         @endif
                         @if(auth()->user()->role_id == 1 | auth()->user()->role_id == 2)
                         <tr>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="px-6 py-4 whitespace-nowrap sm:text-base md:text-sm text-gray-500">
                                 Eventos especiales
                             </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="sm:px-2 md:px-6 py-4 whitespace-nowrap sm:text-base md:text-sm text-gray-500">
                                 <a href="{{route('tabla-eventos-especiales')}}"
-                                    class="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800">Mostrar</a>
+                                    class="inline-flex items-center h-8 px-4 m-2 sm:text-base md:text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800">Mostrar</a>
                             </td>
                         </tr>
                         @endif
                         @if(auth()->user()->role_id == 1 | auth()->user()->role_id == 3)
                         <tr>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="px-6 py-4 whitespace-nowrap sm:text-base md:text-sm text-gray-500">
                                 Extensiones telefónicas
                             </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="sm:px-2 md:px-6 py-4 whitespace-nowrap sm:text-base md:text-sm text-gray-500">
                                 <a href="{{route('tabla-extensiones')}}"
-                                    class="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800">Mostrar</a>
+                                    class="inline-flex items-center h-8 px-4 m-2 sm:text-base md:text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800">Mostrar</a>
                             </td>
                         </tr>
                         <tr>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="px-6 py-4 whitespace-nowrap sm:text-base md:text-sm text-gray-500">
                                 Nivel
                             </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="sm:px-2 md:px-6 py-4 whitespace-nowrap sm:text-base md:text-sm text-gray-500">
                                 <a href="{{route('tabla-niveles')}}"
-                                    class="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800">Mostrar</a>
+                                    class="inline-flex items-center h-8 px-4 m-2 sm:text-base md:text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800">Mostrar</a>
                             </td>
                         </tr>
                         <tr>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="px-6 py-4 whitespace-nowrap sm:text-base md:text-sm text-gray-500">
                                 Puesto
                             </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            <td class="sm:px-2 md:px-6 py-4 whitespace-nowrap sm:text-base md:text-sm text-gray-500">
                                 <a href="{{route('tabla-puestos')}}"
-                                    class="inline-flex items-center h-8 px-4 m-2 text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800">Mostrar</a>
+                                    class="inline-flex items-center h-8 px-4 m-2 sm:text-base md:text-sm text-indigo-100 transition-colors duration-150 bg-blue-700 rounded-lg focus:shadow-outline hover:bg-blue-800">Mostrar</a>
                             </td>
                         </tr>
                         @endif
