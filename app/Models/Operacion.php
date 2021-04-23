65532a6426a47976490bd373fbe16a84cbdea14b
Alexxlml <inkisidor_117@hotmail.com>
2021-04-23 12:17:53 -0500

    Cambios en modelo Colaborador y Operacion
    - Se agregaron las relaciones uno a muchos y muchos a uno entre los modelos Colaborador y Operacion.
    
    - Se agregó el campo tipo_contrato_id en el modelo Colaborador

--- app/Models/Operacion.php
+++ app/Models/Operacion.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Operacion extends Model
 {
     use HasFactory;
+
+    protected $table = 'operacion';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'operacion',
+        'proceso'
+    ];
+
+    // * Relacion uno a muchos
+    public function colaboradores()
+    {
+        return $this->hasMany('App\Models\Colaborador');
+    }
 }
