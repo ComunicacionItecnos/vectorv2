2c7313f4b8705b61fcd30b1510d27e39ade6aad1
Alexxlml <inkisidor_117@hotmail.com>
2021-04-23 12:27:35 -0500

    Cambios en modelo Colaborador y en Tipo_Contrato
    - Se agregó la relación uno a muchos en Modelo Tipo_contrato
    
    - Se agregó la relación muchos a uno en el Modelo Colaborador
    
    - Se agregó el campo tipo_contrato_id a la propiedad $fillable del modelo Colaborador

--- app/Models/Tipo_contrato.php
+++ app/Models/Tipo_contrato.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Tipo_contrato extends Model
 {
     use HasFactory;
+
+    protected $table = 'tipo_contrato';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'contrato',
+    ];
+
+    // * Relacion uno a muchos
+    public function colaboradores()
+    {
+        return $this->hasMany('App\Models\Colaborador');
+    }
 }
