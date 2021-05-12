e2cd0102cf082b4c75ffae1bbd4ae5e97c7fc2da
Alexxlml <inkisidor_117@hotmail.com>
2021-05-12 08:31:30 -0500

    Cambios en FormularioContrato
    - Se creó el componente livewire y la vista para FormularioContrato
    
    - Se añadió un icono para redirigir hacía el formulario del contrato en la tabla de colaboradores

--- resources/views/livewire/colaboradores-tabla.blade.php
+++ resources/views/livewire/colaboradores-tabla.blade.php
@@ -665,6 +665,15 @@
                                     </a>
                                 </div>
 
+                                <div class="w-4 pb-1 mr-6 transform hover:text-indigo-500 hover:scale-130">
+                                    <a href="{{ url('/edit/' . $colaborador->no_colaborador) }}">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                                        </svg>
+                                    </a>
+                                </div>
+
                                 <div class="w-4 mr-2 transform hover:text-red-500 hover:scale-130">
                                     <button wire:click="baja({{ $colaborador->no_colaborador }})" type="submit">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
