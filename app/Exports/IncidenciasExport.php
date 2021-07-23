5fe2019ce80e3590ee3ca521eb410dae98bdca87
Alexxlml <inkisidor_117@hotmail.com>
2021-07-23 17:10:37 -0500

    Cambios en ExportIncidencias
    - Se agregaron los encabezado del excel de incidencias así como sus referencias a la variable que viene desde el constructor

--- app/Exports/IncidenciasExport.php
+++ app/Exports/IncidenciasExport.php
@@ -2,15 +2,40 @@
 
 namespace App\Exports;
 
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class IncidenciasExport implements FromCollection
+class IncidenciasExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
+
+    use Exportable;
+
+    protected $incidencia;
+
+    public function __construct($incidencia)
+    {
+        $this->incidencia = $incidencia;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'No. Colaborador',
+            'Nombre',
+            'Puesto',
+            'Area',
+            'Tipo de incidencia',
+            'Fecha de incidencia'
+        ];
+    }
+
     public function collection()
     {
-        //
+        return $this->incidencia;
     }
 }
