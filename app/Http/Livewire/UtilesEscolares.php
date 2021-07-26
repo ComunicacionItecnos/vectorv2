93f478e9fe6f5e3b72d59325d6c01eaa4701d885
Alexxlml <inkisidor_117@hotmail.com>
2021-07-26 09:45:37 -0500

    Varios arreglos en el componente utilesescolares
    - Se cambió la variable "$no_colaborador" a "$colaborador" en esta variable se guardaba el perfil completo del colaborador a lo cual se prestaba a confusiones al usar $no_colaborador->no_colaborador para hacer referencia a esta propiedad del colaborador.
    
    - Se corrigió la consulta a la tabla de utiles escolares en el método mount.
    
    - Se corrigieron las sentencias insert de ambos casos de numero de kits, se usaba un query de inserción con el cual no se insertaban los timestamps, con el uso del modelo de utiles escolares se corrigió este problema.
    
    - Se formateó el código para hacerlo un poco más legible

--- app/Http/Livewire/UtilesEscolares.php
+++ app/Http/Livewire/UtilesEscolares.php
@@ -3,88 +3,95 @@
 namespace App\Http\Livewire;
 
 use App\Models\Colaborador;
+use App\Models\utilesEscolares as ModelsUtilesEscolares;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class UtilesEscolares extends Component
 {
 
-    public $no_colaborador,$utiles;
-    public $escolaridad_id1,$escolaridad_id2,$escolaridad;
+    public $colaborador, $utiles;
+    public $escolaridad_id1, $escolaridad_id2, $escolaridad;
     public $no_kits;
 
-    public function mount($no_colaborador){
-        $this->no_colaborador= Colaborador::find($no_colaborador);
-        $this->utiles = DB::select('SELECT colaborador_no_colaborador FROM utiles_escolares WHERE colaborador_no_colaborador ='.$this->no_colaborador->no_colaborador);
+    public function mount($no_colaborador)
+    {
+        $this->colaborador = Colaborador::find($no_colaborador);
+        $this->utiles = DB::table('utiles_escolares')->where('colaborador_no_colaborador', $this->colaborador->no_colaborador)->get();
     }
 
-    public function render(){
-        $this->escolaridad= DB::select('SELECT * FROM escolaridad WHERE escolaridad.id=2 || escolaridad.id=3');
+    public function render()
+    {
+        $this->escolaridad = DB::select('SELECT * FROM escolaridad WHERE escolaridad.id=2 || escolaridad.id=3');
         return view('livewire.utiles-escolares')->layout('layouts.guest');
     }
 
-    public function triggerConfirm(){ 
-        if ($this->no_kits=='' || empty($this->no_kits)) {
-            $validateData= $this->validate(
+    public function triggerConfirm()
+    {
+        if ($this->no_kits == '' || empty($this->no_kits)) {
+            $validateData = $this->validate(
                 [
-                    'no_kits'=>'required'
+                    'no_kits' => 'required'
                 ],
                 [
-                    'no_kits.required'=>'Este campo no puede permanecer vacio'
+                    'no_kits.required' => 'Este campo no puede permanecer vacio'
                 ]
             );
         }
 
+        // ? Insert para un kit
+
         if ($this->no_kits == 1) {
-            $validateData= $this->validate(
+            $validateData = $this->validate(
                 [
-                    'escolaridad_id1'=>'required'
+                    'escolaridad_id1' => 'required'
                 ],
                 [
-                    'escolaridad_id1.required'=>'Este campo no puede permanecer vacio',
+                    'escolaridad_id1.required' => 'Este campo no puede permanecer vacio',
                 ]
             );
 
-            $comprobar=DB::table('utiles_escolares')->insert([
-                'colaborador_no_colaborador'=>$this->no_colaborador->no_colaborador,
-                'escolaridad_id'=>$validateData['escolaridad_id1']
+            $comprobar = ModelsUtilesEscolares::create([
+                'colaborador_no_colaborador' => $this->colaborador->no_colaborador,
+                'escolaridad_id' => $validateData['escolaridad_id1'],
             ]);
+
             if ($comprobar) {
-                $this->no_kits='';
-                $this->escolaridad_id1='';
-                if (count($this->utiles) >=0 || count($this->utiles) <=1 ) {
-                    return redirect()->to('/utiles-escolares/'.$this->no_colaborador->no_colaborador);
+                $this->no_kits = '';
+                $this->escolaridad_id1 = '';
+                if (count($this->utiles) >= 0 || count($this->utiles) <= 1) {
+                    return redirect()->to('/utiles-escolares/' . $this->colaborador->no_colaborador);
                 }
             }
         }
 
-        if ($this->no_kits ==2 ) {        
-            $validateData= $this->validate(
+        // ? Insert para un kit
+
+        if ($this->no_kits == 2) {
+            $validateData = $this->validate(
                 [
-                    'escolaridad_id1'=>'required',
-                    'escolaridad_id2'=>'required'
+                    'escolaridad_id1' => 'required',
+                    'escolaridad_id2' => 'required'
                 ],
                 [
-                    'escolaridad_id1.required'=>'Este campo no puede permanecer vacio',
-                    'escolaridad_id2.required'=>'Este campo no puede permanecer vacio'
+                    'escolaridad_id1.required' => 'Este campo no puede permanecer vacio',
+                    'escolaridad_id2.required' => 'Este campo no puede permanecer vacio'
                 ]
             );
-            
-            $comprobar=DB::table('utiles_escolares')->insert([
-                [ 'colaborador_no_colaborador'=>$this->no_colaborador->no_colaborador, 'escolaridad_id'=>$validateData['escolaridad_id1'] ],
-                [ 'colaborador_no_colaborador'=>$this->no_colaborador->no_colaborador, 'escolaridad_id'=>$validateData['escolaridad_id2'] ],
+
+            $comprobar = ModelsUtilesEscolares::create([
+                ['colaborador_no_colaborador' => $this->colaborador->no_colaborador, 'escolaridad_id' => $validateData['escolaridad_id1']],
+                ['colaborador_no_colaborador' => $this->colaborador->no_colaborador, 'escolaridad_id' => $validateData['escolaridad_id2']],
             ]);
 
             if ($comprobar) {
                 // dd('Se inserto');
-                $this->no_kits='';
-                $this->escolaridad_id1='';
-                $this->escolaridad_id2='';
-                
-                return redirect()->to('/utiles-escolares/'.$this->no_colaborador->no_colaborador);
+                $this->no_kits = '';
+                $this->escolaridad_id1 = '';
+                $this->escolaridad_id2 = '';
+
+                return redirect()->to('/utiles-escolares/' . $this->colaborador->no_colaborador);
             }
-        } 
-
+        }
     }
-
 }
