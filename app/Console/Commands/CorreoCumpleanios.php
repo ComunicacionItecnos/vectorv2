d2ed32fab76a7288918738eed5d8520c165d9a1f
Alexxlml <inkisidor_117@hotmail.com>
2021-07-29 14:55:31 -0500

    Creación de método para la tarea de correo Cumpleaños
    - Se agregó la lógica de consulta y envío de información para la generación de correos de cumpleaños

--- app/Console/Commands/CorreoCumpleanios.php
+++ app/Console/Commands/CorreoCumpleanios.php
@@ -2,7 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Mail\NotificaCumpleanios;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Storage;
 
 class CorreoCumpleanios extends Command
 {
@@ -32,11 +37,26 @@ class CorreoCumpleanios extends Command
 
     /**
      * Execute the console command.
-     *
+     *8
      * @return int
      */
     public function handle()
     {
-        return 0;
+        $cumpleDia = DB::table('cumples')
+            ->where('cumple', Carbon::now()->isoFormat('MM-D'))
+            ->get();
+
+        foreach ($cumpleDia as $key) {
+
+            $colaborador = DB::table('colaborador')
+                ->where('no_colaborador', $key->no_colaborador)
+                ->get();
+
+            $col = $colaborador[0];
+
+            Mail::to($col->correo)->send(new NotificaCumpleanios(
+                $col
+            ));
+        }
     }
 }
