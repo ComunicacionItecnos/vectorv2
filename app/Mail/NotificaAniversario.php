9a065e21c541545cff0aa10154fbbda839230441
Alexxlml <inkisidor_117@hotmail.com>
2021-08-02 17:09:36 -0500

    Modificación notifica Aniversario
    - Se agregó sendgrid como servicio de envío de correos a la clase Mail de aniversario

--- app/Mail/NotificaAniversario.php
+++ app/Mail/NotificaAniversario.php
@@ -6,10 +6,11 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Sichikawa\LaravelSendgridDriver\SendGrid;
 
 class NotificaAniversario extends Mailable
 {
-    use Queueable, SerializesModels;
+    use Sendgrid;
 
     /**
      * Create a new message instance.
