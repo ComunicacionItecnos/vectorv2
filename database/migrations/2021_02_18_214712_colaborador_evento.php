5dd4e2522c8ed2d58f0aa0e652f1c5a006f9bc99
Alexxlml <inkisidor_117@hotmail.com>
2021-08-04 09:36:50 -0500

    Cambios en colaborador_evento
    - Se agregó el campo id a la tabla colaborador evento ya que este no tenía

--- database/migrations/2021_02_18_214712_colaborador_evento.php
+++ database/migrations/2021_02_18_214712_colaborador_evento.php
@@ -14,6 +14,7 @@ class ColaboradorEvento extends Migration
     public function up()
     {
         Schema::create('colaborador_evento', function (Blueprint $table){
+            $table->id();
             $table->string('colaborador_no_colaborador', 6);
             $table->foreign('colaborador_no_colaborador')
                     ->references('no_colaborador')->on('colaborador')
