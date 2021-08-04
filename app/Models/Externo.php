024968ca35b7c927b31193fcbebb8b2ed9e91c04
Alexxlml <inkisidor_117@hotmail.com>
2021-08-04 11:56:17 -0500

    Creación de migración y modelo de colaborador externo
    - Se creó la migración y modelo del colaborador externo que contendrá la información clave para el registro y conocimiento de los colaboradores, visitantes y proveedores que no pertenecen directamente a la empresa

---
+++ app/Models/Externo.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Externo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'externo';
+
+    protected $fillable = [
+        'nombre_1',
+        'nombre_2',
+        'ap_paterno',
+        'ap_materno',
+        'fecha_nacimiento',
+        'genero_id',
+        'curp',
+        'rfc',
+        'area_id',
+        'supervisor',
+        'tipo_externo_id',
+        'tel_contacto',
+        'foto'
+    ];
+
+    //* Relacion muchos a uno
+
+    public function generos()
+    {
+        return $this->belongsTo('App\Models\Genero');
+    }
+
+    public function areas()
+    {
+        return $this->belongsTo('App\Models\Area');
+    }
+
+    public function colaboradores()
+    {
+        return $this->belongsTo('App\Models\Colaborador');
+    }
+
+    public function tipos_externo()
+    {
+        return $this->belongsTo('App\Models\Tipo_externo');
+    }
+}
---
+++ database/migrations/2021_08_04_094553_externo.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Externo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('externo', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_1', 24);
+            $table->string('nombre_2', 24);
+            $table->string('ap_paterno', 24);
+            $table->string('ap_materno', 24);
+            $table->date('fecha_nacimiento');
+
+            $table->foreignId('genero_id')
+                ->constrained('genero')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->string('curp', 18);
+            $table->string('rfc', 15);
+
+            $table->foreignId('area_id')
+                ->constrained('area')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->string('supervisor', 6);
+            $table->foreign('supervisor')
+                ->references('no_colaborador')->on('colaborador')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreignId('tipo_externo_id')
+                ->constrained('tipo_externo')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->string('tel_contacto', 10);
+
+            $table->string('foto', 255);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('externo');
+    }
+}
