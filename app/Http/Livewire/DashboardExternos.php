d4c8d84b2beb54e0acefc24af9d392a22371c8de
Alexxlml <inkisidor_117@hotmail.com>
2021-08-05 10:46:23 -0500

    Creación de componente y vista para colaboradores externos
    - Se creó el componente y vista para el colaborador externo, de momento no tiene información adicional más que un texto de prueba en caso de la vista.
    
    - Creación de vista provisional de apoyo a la principal para el colaborado externo la cual contiene la envoltura de la página y los encabezados personalizados

---
+++ app/Http/Livewire/DashboardExternos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DashboardExternos extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard-externos');
+    }
+}
