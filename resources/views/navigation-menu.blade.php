841ff54232f02b694849a601f6c5218f8a158dc3
Alexxlml <inkisidor_117@hotmail.com>
2021-08-09 16:58:18 -0500

    Cambios en navigation-bar
    - Se agregaron los accesos directos agrupados para el listado de vehiculos internos y externos

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -53,11 +53,6 @@ $nav_links6_5 = [
 'active' => request()->routeIs('control-center'),
 ],
 [
-'name' => 'Lista Vehículos',
-'route' => route('lista-vehiculos'),
-'active' => request()->routeIs('lista-vehiculos'),
-],
-[
 'name' => 'Incidencias',
 'route' => route('incidencias'),
 'active' => request()->routeIs('incidencias'),
@@ -143,6 +138,28 @@ $nav_links9 = [
                                     {{ __('Externo') }}
                                 </x-jet-dropdown-link>
 
+                            </x-slot>
+
+                        </x-jet-dropdown>
+                    </div>
+                    @endif
+                    @if(auth()->user()->role_id == 6)
+                    <div class="hidden sm:flex sm:items-center sm:ml-6">
+                        <x-jet-dropdown align="right">
+                            <x-slot name="trigger">
+                                <button type="button"
+                                    class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                                    Lista vehículos
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-jet-dropdown-link href="{{  route('lista-vehiculos') }}">
+                                    {{ __('Internos') }}
+                                </x-jet-dropdown-link>
+                                <x-jet-dropdown-link href="{{  route('lista-vehiculos-externos') }}">
+                                    {{ __('Externos') }}
+                                </x-jet-dropdown-link>
                             </x-slot>
 
                         </x-jet-dropdown>
@@ -391,7 +408,7 @@ $nav_links9 = [
                 <div class="flex items-center px-4">
                     <div class="text-base font-medium text-gray-800">{{ __('Crear colaborador') }}</div>
                 </div>
-            
+
                 @if(auth()->user()->role_id == 1 || auth()->user()->role_id == 3)
                 <div class="mt-3 space-y-1">
                     <!-- Account Management -->
@@ -404,6 +421,27 @@ $nav_links9 = [
                     <!-- Account Management -->
                     <x-jet-responsive-nav-link href="{{  route('registro-externos') }}">
                         {{ __('- Colaborador Externo') }}
+                    </x-jet-responsive-nav-link>
+                </div>
+            </div>
+            @endif
+
+            @if(auth()->user()->role_id == 6)
+            <div class="pt-4 pb-1 border-t border-gray-200">
+                <div class="flex items-center px-4">
+                    <div class="text-base font-medium text-gray-800">{{ __('Lista vehículos') }}</div>
+                </div>
+                <div class="mt-3 space-y-1">
+                    <!-- Account Management -->
+                    <x-jet-responsive-nav-link href="{{  route('lista-vehiculos') }}">
+                        {{ __('- Internos') }}
+                    </x-jet-responsive-nav-link>
+                </div>
+
+                <div class="mt-3 space-y-1">
+                    <!-- Account Management -->
+                    <x-jet-responsive-nav-link href="{{  route('lista-vehiculos-externos') }}">
+                        {{ __('- Externos') }}
                     </x-jet-responsive-nav-link>
                 </div>
             </div>
