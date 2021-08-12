8849c7fac8803c0c86838b7162a11a4252fe3d34
Alexxlml <inkisidor_117@hotmail.com>
2021-08-12 11:43:59 -0500

    Creación de migraciones y modelos para modulo uniformes
    - Se crearon las migraciones y sus respectivos modelos para hacer la conexión entre tablas que harán posible el resgistro de tallas de uniformes del personal Aguila

---
+++ app/Models/Uniformes_paquete_prenda.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Uniformes_paquete_prenda extends Model
+{
+    use HasFactory;
+}
