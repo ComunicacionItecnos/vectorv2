b7b470c19dabf47dcf1990c59b581e56c382520b
Alexxlml <inkisidor_117@hotmail.com>
2021-08-24 10:45:00 -0500

    Cambios en los intentos
    - Se cambió el orden de los intentos para 3 de los usuarios. pasó de 2 oro, 2 plata y 1 bronce a 1 oro, 2 plata y 1 bronce.

--- app/Http/Livewire/Insignias.php
+++ app/Http/Livewire/Insignias.php
@@ -382,9 +382,9 @@ class Insignias extends Component
             auth()->user()->colaborador_no_colaborador == '131901' ||
             auth()->user()->colaborador_no_colaborador == '152090'
         ) {
-            $this->intentoOro = 2;
+            $this->intentoOro = 1;
             $this->intentoPlata = 2;
-            $this->intentoBronce = 1;
+            $this->intentoBronce = 2;
         } else {
             $this->intentoOro = 1;
             $this->intentoPlata = 1;
