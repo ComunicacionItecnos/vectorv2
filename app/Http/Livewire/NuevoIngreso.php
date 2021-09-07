bcc6edbbaaf9e019d000288ceed30b64e396d4c6
AndresDomCas <t.d.w.p_ff@hotmail.com>
2021-09-07 17:46:54 -0500

    Se removio insert
    Se removio el insert en la tabla revision_doc

--- app/Http/Livewire/NuevoIngreso.php
+++ app/Http/Livewire/NuevoIngreso.php
@@ -587,10 +587,6 @@ class NuevoIngreso extends Component
                 ],
             ]);
 
-            DB::table('revision_docs')->insert([
-                'nuevo_ingreso_id'=>$nuevo_ingreso->id
-            ]);
-
         });
 
         
