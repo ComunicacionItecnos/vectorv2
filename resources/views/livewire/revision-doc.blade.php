034dea5d1cbd02b4e214204333874fe678181010
AndresDomCas <t.d.w.p_ff@hotmail.com>
2021-09-23 08:37:54 -0500

    solucion obsCartaNopenales

--- resources/views/livewire/revision-doc.blade.php
+++ resources/views/livewire/revision-doc.blade.php
@@ -874,7 +874,7 @@
                         @else
                             <div class="col-span-full sm:col-span-6  @if ($nopenalesValue) hidden @else @endif">
                                 <label for="observacionnopenalesValue" class="text-sm">Observaciones</label>
-                                <textarea id="observacionnopenalesValue" wire:model="observacionnopenalesValue"
+                                <textarea id="observacionnopenalesValue" wire:model="observacionnopenales"
                                     type="text"
                                     class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400 dark:border-coolGray-700 dark:text-coolGray-900"
                                     @if ($nopenalesValue)  @else required @endif></textarea>
@@ -883,10 +883,10 @@
 
                         @if ($userLogin == 5 && $r_obscartaNoPenales != null)
                             <div class="col-span-full sm:col-span-6">
-                                <label for="r_observacionnopenalesValue" class="text-sm"><span
+                                <label for="r_observacionnopenales" class="text-sm"><span
                                         class="text-red-600">*</span>
                                     Observaciones realizadas por reclutamiento</label>
-                                <textarea id="r_observacionnopenalesValue" type="text"
+                                <textarea id="r_observacionnopenales" type="text"
                                     class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400 dark:border-coolGray-700 dark:text-coolGray-900"
                                     disabled>{{ $r_obscartaNoPenales }}</textarea>
                             </div>
@@ -895,10 +895,10 @@
                         @if ($userLogin == 5 && $a_obscartaNoPenales != null)
 
                             <div class="col-span-full sm:col-span-6">
-                                <label for="a_observacionnopenalesValue" class="text-sm"><span
+                                <label for="a_observacionnopenales" class="text-sm"><span
                                         class="text-red-600">*</span> Observaciones realizadas por
                                     administración</label>
-                                <textarea id="a_observacionnopenalesValue" type="text"
+                                <textarea id="a_observacionnopenales" type="text"
                                     class="w-full rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400 dark:border-coolGray-700 dark:text-coolGray-900"
                                     disabled>{{ $a_obscartaNoPenales }}</textarea>
                             </div>
