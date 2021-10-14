a3209c65895bc3b1c84a0e346b86f64c65bbb752
AndresDomCas <t.d.w.p_ff@hotmail.com>
2021-10-14 15:50:45 -0500

    Arreglo de las actas y cartas de recomendacion

--- app/Http/Livewire/NuevoIngreso.php
+++ app/Http/Livewire/NuevoIngreso.php
@@ -514,8 +514,8 @@ class NuevoIngreso extends Component
                 }else{
                         for ($i=0; $i < count($this->actasHijo) ; $i++) { 
                             $rutaActasHijos2 = $this->actasHijo[$i]->storeAs('public/nuevoIngreso/'.$this->curp.'/08.-actasHijos','actaDeHijo'.$i.'.pdf');
-                            $rutaActaHijo = array();
-                            $rutaActaHijo = $rutaActasHijos2;
+                            /* $rutaActaHijo = array(); */
+                            $rutaActaHijo[] = $rutaActasHijos2;
                         }
                 }
                     
@@ -524,8 +524,8 @@ class NuevoIngreso extends Component
                 }else{
                         for ($i=0; $i < count($this->cartasRecomendacion) ; $i++) { 
                             $rutaRecomendacion2 = $this->cartasRecomendacion[$i]->storeAs('public/nuevoIngreso/'.$this->curp.'/09.-cartasRecomendacion','cartaDeRecomendacion'.$i.'.pdf');
-                            $rutaRecomendacion = array();
-                            $rutaRecomendacion = $rutaRecomendacion2; 
+                            /* $rutaRecomendacion = array(); */
+                            $rutaRecomendacion[] = $rutaRecomendacion2; 
                         }
                 }
 
