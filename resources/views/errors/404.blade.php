8efd4fba885d4bfc35007d297fad3366be0f15be
AndresDomCas <t.d.w.p_ff@hotmail.com>
2022-03-17 09:23:17 -0600

    Error 404
    -Se modifico el error 404 para mostrar img para disc

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('errors::minimal')
+
+{{-- @section('title', __('Not Found'))
+@section('code', '404')
+@section('message', __('Not Found')) --}}
+
+@section('img')
+<img src="{{ asset('images/disc/pantalla.jpg') }}" />
+@endsection
